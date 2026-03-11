fec1f593687dabc6f370976a65bfd42ea14f16bc
Piper Wolf <piper.w.wolf@gmail.com>
2026-03-10 21:33:01 -0700

    Migrate to react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Piper Wolf</h1>
+      <p>The only way to learn is by playing. The only way to win is by learning. And the only way to begin is by beginning.</p>
+      <img src="frog-spin.gif"/>
+    </>
+  )
+}
+
+export default App
