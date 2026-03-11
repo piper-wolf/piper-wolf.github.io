7f4c8698614fb7c73c28fcb242d3256c1668fb8d
Piper Wolf <piper.w.wolf@gmail.com>
2026-03-10 22:18:00 -0700

    Remove app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <div
-        css={{
-          backgroundColor: "hotpink",
-          "&:hover": {
-            color: "lightgreen",
-          },
-        }}
-      >
-        This has a hotpink background.
-      </div>
+    <div
+      css={{
+        maxWidth: "1280px",
+        margin: "0 auto",
+        textAlign: "center",
+        height: "100%",
+        alignContent: "space-around",
+      }}
+    >
       <h1 css={{ color: "pink" }}>Piper Wolf</h1>
       <p>
         The only way to learn is by playing. The only way to win is by learning.
         And the only way to begin is by beginning.
       </p>
       <img src="frog-spin.gif" />
-    </>
+    </div>
   );
 }
 
