60ac05ddb3739e42a3fd0c170b5a56e5c508e027
Piper Wolf <piper.w.wolf@gmail.com>
2026-03-10 22:00:04 -0700

    Install emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Piper Wolf</h1>
-      <p>The only way to learn is by playing. The only way to win is by learning. And the only way to begin is by beginning.</p>
-      <img src="frog-spin.gif"/>
+      <div
+        css={{
+          backgroundColor: "hotpink",
+          "&:hover": {
+            color: "lightgreen",
+          },
+        }}
+      >
+        This has a hotpink background.
+      </div>
+      <h1 css={{ color: "pink" }}>Piper Wolf</h1>
+      <p>
+        The only way to learn is by playing. The only way to win is by learning.
+        And the only way to begin is by beginning.
+      </p>
+      <img src="frog-spin.gif" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
